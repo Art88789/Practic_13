46a3a853ff7a4593a782235e27286e0aff4c1771
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-16 10:51:23 +0700

    1.py

--- 1.py
+++ 1.py
@@ -1,26 +1,12 @@
 def check_repeated_number():
-    """Проверяет, принадлежит ли заданное число множеству повторяющихся чисел в последовательности."""
     numbers = list(map(int, input().split()))
     target = int(input())
-
-    # Считаем, сколько раз встречается каждое число
-    counts = {}
-    for num in numbers:
-        if num in counts:
-            counts[num] += 1
-        else:
-            counts[num] = 1
-
-    # Находим все числа, которые встречаются больше одного раза
-    repeated = set()
-    for num, count in counts.items():
-        if count > 1:
-            repeated.add(num)
-
-    return target in repeated
-
+    
+    # Считаем количество вхождений target напрямую
+    return numbers.count(target) > 1
 
 if check_repeated_number():
     print("YES")
 else:
     print("NO")
+
