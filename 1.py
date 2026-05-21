79805d71f128159cc461277a4f5b97bcaec2fb5b
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-05-21 13:29:56 +0700

    1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
 def check_repeated_number():
+    """Проверяет, встречается ли введенное число в списке более одного раза."""
     numbers = list(map(int, input().split()))
     target = int(input())
-    
-    # Считаем количество вхождений target напрямую
+
     return numbers.count(target) > 1
+
 
 if check_repeated_number():
     print("YES")
 else:
     print("NO")
-
